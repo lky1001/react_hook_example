77439c012c741f9d6442be16a5245f311fbfc996
lky1001 <lky1001@gmail.com>
2019-03-15 15:58:39 +0900

    Only re-run the effect if count changes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,20 @@ import React, { useState, useEffect } from 'react'
 function Example() {
   // Declare a new state variable, which we'll call "count"
   const [count, setCount] = useState(0)
+  const [state, setState] = useState(0)
 
   useEffect(() => {
     // Update the document title using the browser API
     document.title = `You clicked ${count} times`
-  })
+    console.log('count changed')
+  }, [count])
 
   return (
     <div>
       <p>You clicked {count} times</p>
+      <p>You state clicked {state} times</p>
       <button onClick={() => setCount(count + 1)}>Click me</button>
+      <button onClick={() => setState(state + 1)}>State Click me</button>
     </div>
   )
 }
